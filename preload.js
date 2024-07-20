fdf21c2b129fc86b528c0ddde9d0b80cfd286242
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-07-21 01:50:32 +0300

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { ipcRenderer, contextBridge: { exposeInMainWorld } } = require('electron')
-
-exposeInMainWorld('renderer', {
-    send: (channel, data) => ipcRenderer.send(channel, data),
-    on: (channel, fn) => ipcRenderer.on(channel, (ev, ...args) => fn(...args))
-});
